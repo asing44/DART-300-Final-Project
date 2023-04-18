5e1c4194575e6c1ebd99466d34a905b1bfeddc58
asing44 <109570966+asing44@users.noreply.github.com>
2023-04-18 14:58:54 -0400

    4/18/23

--- scripts.js
+++ scripts.js
@@ -2,16 +2,43 @@ const titleVideo = document.getElementsByTagName("video")[0];
 const titleVideoContainer = document.getElementById("title_intro");
 const mainContainer = document.getElementsByClassName("--main-video-container")[0];
 const mainVideo = document.getElementById("main_video");
+
 const timeline = document.getElementById("timeline_elements");
+
+var currentIndex = 0;
 
 titleVideo.onended = function() {
     titleVideoContainer.classList.add("--video-end");
-    document.body.classList.remove("--lock-scroll");
-}
+    const c = document.getElementsByClassName("--controls");
+    for (i in c) {
+        if (c[i].nodeName) {
+            c[i].onclick = e => {
+                if (e.target.classList.contains("--f")) {
+                    if (currentIndex < 9) {
+                        currentIndex += 1;
+                    } else {
+                        currentIndex = 0;
+                    }
+                } else if (e.target.classList.contains("--b")) {
+                    if (currentIndex > 0) {
+                        currentIndex += -1;
+                    } else {
+                        currentIndex = 9;
+                    }
+                }
+                console.log(currentIndex);
+
+                mainVideo.play();
+                setTimeout(() => {
+                    mainVideo.pause();
+                }, 4160);
+            }
+        }
+    }
+};
 
 window.onload = function() {
     titleVideo.classList.add("--video-start");
-    document.body.classList.add("--lock-scroll");
     mainContainer.classList.add("--fade-opacity-in");
     mainVideo.classList.add("--fade-opacity-in");
     timeline.classList.add("--fade-opacity-in");
