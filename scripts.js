5990029d9e648b94c951ebd1c3a831ec4d4d7412
asing44 <109570966+asing44@users.noreply.github.com>
2023-04-30 16:34:30 -0400

    4/30/23

--- scripts.js
+++ scripts.js
@@ -24,7 +24,7 @@ const b = document.querySelectorAll("span.beginning")[0];
 const t = document.querySelectorAll("span.there")[0];
 const a = document.querySelectorAll("span.and")[0];
 const c = document.querySelectorAll("span.canoe")[0];
-const canoe = document.getElementsByClassName("theCanoe")[0];
+const mainContent = document.getElementsByClassName("section-wrap")[0];
 
 introText.to(".beginning", {
     duration: 4,
@@ -54,7 +54,7 @@ introText.to(".there", {
     onComplete: () => {
         t.style.display = "none";
         a.style.display = "block";
-        canoe.classList.remove("hidden");
+        mainContent.classList.remove("hidden");
     }
 }, ">-=1");
 
@@ -85,3 +85,23 @@ introText.to(".h1-wrap", {
         h1Wrap.style.display = "none";
     }
 }, ">+=1")
+
+
+var slides=document.querySelectorAll('.slide'),tl=gsap.timeline({paused:true});
+for(var i=slides.length;i--;){
+    if(i>0){
+      if(i!=slides.length-1){tl.addPause()}
+        tl.to(slides[i],0.5,{scale:.5,ease:Back.easeInOut})
+        .to(slides[i],0.7,{xPercent:-100},'L'+i)
+        .from(slides[i-1],0.7,{xPercent:100},'L'+i)
+        .set(slides[i],{zIndex:1-i}).set(slides[i-1],{zIndex:slides.length})
+        .from(slides[i-1],0.5,{scale:.5,ease:Back.easeIn})
+    };
+};
+function GO(e){
+  var SD=isNaN(e)?e.wheelDelta||-e.detail:e;
+  if(SD<0){tl.play()}else{tl.reverse()};
+};
+
+document.getElementsByClassName('right')[0].addEventListener("click",function(){GO(-1)});
+document.getElementsByClassName('left')[0].addEventListener("click",function(){GO(1)});
